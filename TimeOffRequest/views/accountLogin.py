fb41d849fbc26dcb865a36ca1b5c598248bc7b0e
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2024-01-17 09:38:20 -0600

    updated homeview to redirect unauthenticated users to the login page. updated forms and account login

--- TimeOffRequest/views/accountLogin.py
+++ TimeOffRequest/views/accountLogin.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from .forms import EmployeeProfileForm
 
 class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
     success_url = reverse_lazy('home')
+
+class EmployeeRegistrationView(CreateView):
+    model = Employee
+    form_class = EmployeeProfileForm
+    template_name = 'accounts/employee_registration.html'
+    success_url = reverse_lazy('login')
